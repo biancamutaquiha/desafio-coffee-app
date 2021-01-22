41209331ba5135308702bfcca84c6086b177bbac
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2021-01-21 23:33:25 -0300

    price_service_spec adjustments

--- spec/price_service_spec.rb
+++ spec/price_service_spec.rb
@@ -1,11 +1,11 @@
 require 'price_service'
 
 describe 'loading lists' do
-    it 'should return price_json from data' do
-      prices = [{"drink_name":"short espresso","prices":{"small":3.0}},{"drink_name":"latte","prices":{"small":3.5,"medium":4.0,"large":4.5}},{"drink_name":"flat white","prices":{"small":3.5,"medium":4.0,"large":4.5}},{"drink_name":"long black","prices":{"small":3.25,"medium":3.5}},{"drink_name":"mocha","prices":{"small":4.0,"medium":4.5,"large":5.0}},{"drink_name":"supermochacrapucaramelcream","prices":{"large":5.0,"huge":5.5,"mega":6.0,"ultra":7.0}}]
+  it 'should return price_json from data' do
+    prices = [{"drink_name":"short espresso","prices":{"small":3.0}},{"drink_name":"latte","prices":{"small":3.5,"medium":4.0,"large":4.5}},{"drink_name":"flat white","prices":{"small":3.5,"medium":4.0,"large":4.5}},{"drink_name":"long black","prices":{"small":3.25,"medium":3.5}},{"drink_name":"mocha","prices":{"small":4.0,"medium":4.5,"large":5.0}},{"drink_name":"supermochacrapucaramelcream","prices":{"large":5.0,"huge":5.5,"mega":6.0,"ultra":7.0}}]
   
-      price_service = PriceService.new
+    price_service = PriceService.new
 
-      expect(price_service.get_prices_list).to eq price.to_json
-    end
+    expect(price_service.get_prices_list).to eq prices.to_json
   end
+end
