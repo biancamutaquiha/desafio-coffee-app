d6107fb9cc89261946457b59595611d215262635
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2021-01-25 09:25:35 -0300

    refactoring tests

--- spec/order_service_spec.rb
+++ spec/order_service_spec.rb
@@ -10,8 +10,12 @@ describe 'loading lists' do
 end
 
 describe 'order service' do
-    it 'should return a list of orders by user' do
-      orders = [
+    before(:each) do
+      @order_service = OrderService.new
+    end
+    
+    before(:all) do
+      @orders = [
         { "user": "coach", "drink": "long black", "size": "medium" },
         { "user": "ellis", "drink": "long black", "size": "small" },
         { "user": "rochelle", "drink": "flat white", "size": "large" },
@@ -19,22 +23,15 @@ describe 'order service' do
         { "user": "zoey", "drink": "long black", "size": "medium" },
         { "user": "zoey", "drink": "short espresso", "size": "small" }
       ]
-    
-      user_order = [
+
+      @user_order = [
         [{"user"=>"coach", "drink"=>"long black", "size"=>"medium"}, {"user"=>"coach", "drink"=>"flat white", "size"=>"large"}], 
         [{"user"=>"ellis", "drink"=>"long black", "size"=>"small"}], 
         [{"user"=>"rochelle", "drink"=>"flat white", "size"=>"large"}], 
         [{"user"=>"zoey", "drink"=>"long black", "size"=>"medium"}, {"user"=>"zoey", "drink"=>"short espresso", "size"=>"small"}]
       ]
-      
-      order_service = OrderService.new
 
-      expect(order_service.get_user_orders(orders.to_json)).to eq user_order
- 
-    end
-
-    it 'should calcute total order by user' do
-      prices = [
+      @prices = [
         { "drink_name": "short espresso", "prices": { "small": 3.03 } },
         { "drink_name": "latte", "prices": { "small": 3.50, "medium": 4.00, "large": 4.50 } },
         { "drink_name": "flat white", "prices": { "small": 3.50, "medium": 4.00, "large": 4.50 } },
@@ -43,38 +40,23 @@ describe 'order service' do
         { "drink_name": "supermochacrapucaramelcream", "prices": { "large": 5.00, "huge": 5.50, "mega": 6.00, "ultra": 7.00 } }
       ]
 
-      user_order = [
-        [{"user"=>"coach", "drink"=>"long black", "size"=>"medium"}, {"user"=>"coach", "drink"=>"flat white", "size"=>"large"}], 
-        [{"user"=>"ellis", "drink"=>"long black", "size"=>"small"}], 
-        [{"user"=>"rochelle", "drink"=>"flat white", "size"=>"large"}], 
-        [{"user"=>"zoey", "drink"=>"long black", "size"=>"medium"}, {"user"=>"zoey", "drink"=>"short espresso", "size"=>"small"}]
-      ]
-
-      total_order = [
+      @total_order = [
         { "user": "coach",    "order_total": 8.00 },
         { "user": "ellis",    "order_total": 3.25 },
         { "user": "rochelle", "order_total": 4.50 },
         { "user": "zoey",     "order_total": 6.53 }
       ]
-
-      order_service = OrderService.new
-      expect(order_service.get_total_orders(user_order, prices.to_json)).to eq total_order.to_json
-
     end
 
-    it 'should calculate drinks prices' do
-      prices = [
-        { "drink_name": "short espresso", "prices": { "small": 3.03 } },
-        { "drink_name": "latte", "prices": { "small": 3.50, "medium": 4.00, "large": 4.50 } },
-        { "drink_name": "flat white", "prices": { "small": 3.50, "medium": 4.00, "large": 4.50 } },
-        { "drink_name": "long black", "prices": { "small": 3.25, "medium": 3.50 } },
-        { "drink_name": "mocha", "prices": { "small": 4.00, "medium": 4.50, "large": 5.00 } },
-        { "drink_name": "supermochacrapucaramelcream", "prices": { "large": 5.00, "huge": 5.50, "mega": 6.00, "ultra": 7.00 } }
-      ]
-      
-      order_service = OrderService.new
+    it 'should return a list of orders by user' do
+      expect(@order_service.get_user_orders(@orders.to_json)).to eq @user_order
+    end
 
-      expect(order_service.calculate_user_drink_price('latte', 'medium', prices.to_json)).to eq 4.00
+    it 'should calcute total order by user' do
+      expect(@order_service.get_total_orders(@user_order, @prices.to_json)).to eq @total_order.to_json
+    end
 
+    it 'should calculate drinks prices' do  
+      expect(@order_service.calculate_user_drink_price('latte', 'medium', @prices.to_json)).to eq 4.00
     end
 end
--- spec/payment_service_spec.rb
+++ spec/payment_service_spec.rb
@@ -10,59 +10,54 @@ describe 'loading prices list' do
 end
 
 describe 'payment service' do
-  it 'should return the total payments by user' do
-      orders = [
-        { "user": "coach", "drink": "long black", "size": "medium" },
-        { "user": "ellis", "drink": "long black", "size": "small" },
-        { "user": "rochelle", "drink": "flat white", "size": "large" },
-        { "user": "coach", "drink": "flat white", "size": "large" },
-        { "user": "zoey", "drink": "long black", "size": "medium" },
-        { "user": "zoey", "drink": "short espresso", "size": "small" }
-      ]
-      
-      payment_list = [
-        { "user": "coach", "amount": 2.50 },
-        { "user": "ellis", "amount": 2.60 },
-        { "user": "rochelle", "amount": 4.50 },
-        { "user": "ellis", "amount": 0.65 }
-      ]
-    
-      
-      payments = [
-        { "user": "coach", "payment_total": 2.50 },
-        { "user": "ellis", "payment_total": 3.25 },
-        { "user": "rochelle", "payment_total": 4.50 },
-        { "user": "zoey", "payment_total": 0.00 }
-      ]
-      
-      payment_service = PaymentService.new
-      expect(payment_service.calculate_user_payment(orders.to_json, payment_list.to_json)).to eq payments.to_json
+  before(:each) do
+    @payment_service = PaymentService.new
   end
 
-    it 'should calculate what each user now owes' do
-      payments = [
-        { "user": "coach", "payment_total": 2.50 },
-        { "user": "ellis", "payment_total": 3.25 },
-        { "user": "rochelle", "payment_total": 4.50 },
-        { "user": "zoey", "payment_total": 0.00 }
-      ]
+  before(:all) do
+  @orders = [
+    { "user": "coach", "drink": "long black", "size": "medium" },
+    { "user": "ellis", "drink": "long black", "size": "small" },
+    { "user": "rochelle", "drink": "flat white", "size": "large" },
+    { "user": "coach", "drink": "flat white", "size": "large" },
+    { "user": "zoey", "drink": "long black", "size": "medium" },
+    { "user": "zoey", "drink": "short espresso", "size": "small" }
+  ]
 
-      total_order = [
-        { "user": "coach",    "order_total": 8.00 },
-        { "user": "ellis",    "order_total": 3.25 },
-        { "user": "rochelle", "order_total": 4.50 },
-        { "user": "zoey",     "order_total": 6.53 }
-      ]
+  @payment_list = [
+    { "user": "coach", "amount": 2.50 },
+    { "user": "ellis", "amount": 2.60 },
+    { "user": "rochelle", "amount": 4.50 },
+    { "user": "ellis", "amount": 0.65 }
+  ]
 
-      balance = [
-        { "user": "coach",    "order_total": 8.00, "payment_total": 2.50, "balance": 5.50 },
-        { "user": "ellis",    "order_total": 3.25, "payment_total": 3.25, "balance": 0.00 },
-        { "user": "rochelle", "order_total": 4.50, "payment_total": 4.50, "balance": 0.00 },
-        { "user": "zoey",     "order_total": 6.53, "payment_total": 0.00, "balance": 6.53 }
-      ]
-      
-      payment_service = PaymentService.new
+  @payments = [
+    { "user": "coach", "payment_total": 2.50 },
+    { "user": "ellis", "payment_total": 3.25 },
+    { "user": "rochelle", "payment_total": 4.50 },
+    { "user": "zoey", "payment_total": 0.00 }
+  ]
 
-      expect(payment_service.calculate_user_balance(total_order.to_json, payments.to_json)).to eq JSON.parse(balance.to_json)
-    end 
+  @total_order = [
+    { "user": "coach",    "order_total": 8.00 },
+    { "user": "ellis",    "order_total": 3.25 },
+    { "user": "rochelle", "order_total": 4.50 },
+    { "user": "zoey",     "order_total": 6.53 }
+  ]
+
+  @balance = [
+    { "user": "coach",    "order_total": 8.00, "payment_total": 2.50, "balance": 5.50 },
+    { "user": "ellis",    "order_total": 3.25, "payment_total": 3.25, "balance": 0.00 },
+    { "user": "rochelle", "order_total": 4.50, "payment_total": 4.50, "balance": 0.00 },
+    { "user": "zoey",     "order_total": 6.53, "payment_total": 0.00, "balance": 6.53 }
+  ]
+  end
+
+  it 'should return the total payments by user' do 
+    expect(@payment_service.calculate_user_payment(@orders.to_json, @payment_list.to_json)).to eq @payments.to_json
+  end
+
+  it 'should calculate what each user now owes' do
+    expect(@payment_service.calculate_user_balance(@total_order.to_json, @payments.to_json)).to eq JSON.parse(@balance.to_json)
+  end 
 end
