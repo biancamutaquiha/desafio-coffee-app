a4374ba0f6de199ec46474d374096dbde34296c9
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2021-01-29 18:00:31 -0300

    adjustments order service

--- lib/services/order_service.rb
+++ lib/services/order_service.rb
@@ -13,52 +13,20 @@ class OrderService
               end 
             end
         end
-        order_list
+        return order_list
     end
-
-    def calculate_total_orders(orders_list)
-    end
-
-    ###########################################################################3
     
-    def get_total_orders(orders_list, price_list)
-        user_order_list = get_user_orders(orders_list, price_list)
-
-        total_order_list = []
+    def get_total_orders(order_list)
         total_order_hash = {}
     
-        user_order_list.each do |user_order|
-            total_order = 0
-            
-            user_order.each do |user|
-                total_order_hash['user'] = user['user']
-                total_order += calculate_user_drink_price(user['drink'], user['size'], price_list)
-            end
-
-            total_order_hash['order_total'] = total_order.round(2)
-            total_order_list.push(JSON.parse(total_order_hash.to_json))
-        end
-        
-        return total_order_list.to_json
-    end
-
-    private
-    def get_user_orders(orders_list, price_list)
-        user_name_list = JSON.parse(orders_list).map { |order| order['user']}.uniq
-        
-        user_order_list = user_name_list.map do |user_name|
-            user_order = JSON.parse(orders_list).select {|order| order['user'] == user_name}
-        end
-
-        return user_order_list
-    end
-
-    def calculate_user_drink_price(drink, size, prices_list)       
-        JSON.parse(prices_list).each do |price|    
-            if price['drink_name'] == drink
-                return price['prices'][size]
+        order_list.each do |user_order|
+            if total_order_hash.include?(user_order.user_name)
+                total_order_hash[user_order.user_name] += user_order.drink.price.prices[user_order.size]
+            else
+                total_order_hash[user_order.user_name] = user_order.drink.price.prices[user_order.size] 
             end
         end
+        return total_order_hash
     end
 
 end
--- spec/services/order_service_spec.rb
+++ spec/services/order_service_spec.rb
@@ -7,33 +7,20 @@ describe 'order service' do
     end
     
     before(:all) do
-      @orders = [
-        { "user": "coach", "drink": "long black", "size": "medium" },
-        { "user": "ellis", "drink": "long black", "size": "small" },
-        { "user": "rochelle", "drink": "flat white", "size": "large" },
-        { "user": "coach", "drink": "flat white", "size": "large" },
-        { "user": "zoey", "drink": "long black", "size": "medium" },
-        { "user": "zoey", "drink": "short espresso", "size": "small" }
-      ]
+      order_1 = Order.new('coach', Drink.new({ "drink_name"=> "long black", "prices"=> { "small"=> 3.25, "medium"=> 3.50 }}), 'medium')
+      order_2 = Order.new('ellis', Drink.new({ "drink_name"=> "long black", "prices"=> { "small"=> 3.25, "medium"=> 3.50 }}), 'small')
+      order_3 = Order.new('rochelle', Drink.new({ "drink_name"=> "flat white", "prices"=> { "small"=> 3.50, "medium"=> 4.00, "large"=> 4.50 }}), 'large')
+      order_4 = Order.new('coach', Drink.new({ "drink_name"=> "flat white", "prices"=> { "small"=> 3.50, "medium"=> 4.00, "large"=> 4.50 }}), 'large')
+      order_5 = Order.new('zoey', Drink.new({ "drink_name"=> "long black", "prices"=> { "small"=> 3.25, "medium"=> 3.50 }}), 'medium')
+      order_6 = Order.new('zoey', Drink.new({ "drink_name"=> "short espresso", "prices"=> { "small"=> 3.03}}), 'small')
 
-      @prices = [
-        { "drink_name": "short espresso", "prices": { "small": 3.03 } },
-        { "drink_name": "latte", "prices": { "small": 3.50, "medium": 4.00, "large": 4.50 } },
-        { "drink_name": "flat white", "prices": { "small": 3.50, "medium": 4.00, "large": 4.50 } },
-        { "drink_name": "long black", "prices": { "small": 3.25, "medium": 3.50 } },
-        { "drink_name": "mocha", "prices": { "small": 4.00, "medium": 4.50, "large": 5.00 } },
-        { "drink_name": "supermochacrapucaramelcream", "prices": { "large": 5.00, "huge": 5.50, "mega": 6.00, "ultra": 7.00 } }
-      ]
+      @order_list = [order_1, order_2, order_3, order_4, order_5, order_6]
 
-      @total_order = [
-        { "user": "coach",    "order_total": 8.00 },
-        { "user": "ellis",    "order_total": 3.25 },
-        { "user": "rochelle", "order_total": 4.50 },
-        { "user": "zoey",     "order_total": 6.53 }
-      ]
+      @total_order = { "coach" => 8.00, "ellis" => 3.25, "rochelle"=> 4.50, "zoey" => 6.529999999999999}
+      
     end
 
     it 'should calcute total order by user' do
-      expect(@order_service.get_total_orders(@orders.to_json, @prices.to_json)).to eq @total_order.to_json
+      expect(@order_service.get_total_orders(@order_list)).to eq @total_order
     end
 end
