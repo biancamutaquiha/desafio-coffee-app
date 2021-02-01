d0adb5baa1fd11178be9024c7b4a5005929bfd10
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2021-02-01 17:27:07 -0300

    add order serialized test

--- lib/serialize/order_serialize.rb
+++ lib/serialize/order_serialize.rb
@@ -1,12 +1,12 @@
 require 'models/order'
 class OrderSerialize   
-  def get_orders_list(orders_json, drink_list)
+  def get_orders_list(orders_json, product_list)
     order_list = []
 
     JSON.parse(orders_json).each do |order| 
-      drink_list.each do |drink|
-        if (order['drink']) == drink.name
-            order_list.push(Order.new(order['user'], drink, order['size']))
+      product_list.each do |product|
+        if (order[product.class.to_s.downcase]) == product.name
+            order_list.push(Order.new(order['user'], product, order['size']))
         end 
       end
     end
--- spec/serialize/order_serialize_spec.rb
+++ spec/serialize/order_serialize_spec.rb
@@ -1,4 +1,24 @@
 require 'serialize/order_serialize'
 
-describe 'order service' do
+describe 'order deserialize' do
+  it 'should return deserialized order object' do
+    orders_json = [
+        { "user": "coach", "drink": "long black", "size": "medium" },
+        { "user": "ellis", "drink": "long black", "size": "small" },
+        { "user": "rochelle", "drink": "flat white", "size": "large" }
+    ]
+
+    drink_1 = Drink.new({ "drink_name"=> "long black", "prices"=> { "small"=> 3.25, "medium"=> 3.50 }})
+    drink_2 = Drink.new({ "drink_name"=> "flat white", "prices"=> { "small"=> 3.50, "medium"=> 4.00, "large"=> 4.50 }})
+
+    order_serialize = OrderSerialize.new 
+    orders = order_serialize.get_orders_list(orders_json.to_json, [drink_1, drink_2])
+
+    expect(orders[0].user_name).to eq 'coach'
+    expect(orders[0].product.name).to eq 'long black'
+    expect(orders[1].user_name).to eq 'ellis'
+    expect(orders[1].product.name).to eq 'long black'
+    expect(orders[2].user_name).to eq 'rochelle'
+    expect(orders[2].product.name).to eq 'flat white'
+  end
 end
