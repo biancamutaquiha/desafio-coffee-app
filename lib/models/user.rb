8e324085d6a2e9cdd667397f3669a345c2e1be41
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2021-02-09 17:31:42 -0300

    refactoring

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -8,22 +8,14 @@ class User
   end
 
   def set_user_order_list(orders_list)
-    user_order_list = []
-
     orders_list.each do |order| 
-      user_order_list.push(order) if @name == order.user_name
+      @order_list.push(order) if @name == order.user_name
     end
-    
-    @order_list = user_order_list
   end
 
   def set_user_payment_list(payment_list)
-    user_payment_list = []
-
     payment_list.each do |payment| 
-      user_payment_list.push(payment) if @name == payment.user_name
+      @payment_list.push(payment) if @name == payment.user_name
     end
-
-    @payment_list = user_payment_list
   end
 end
